0eb3d10e0fc410de85c65b2c7ffb58260358a96b
jungsoyoun <106327901+jungsoyoun@users.noreply.github.com>
2022-11-19 15:45:22 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/moonky_rider/MainActivity.java
+++ app/src/main/java/com/example/moonky_rider/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
